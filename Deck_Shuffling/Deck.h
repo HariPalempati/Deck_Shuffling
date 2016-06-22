612f588b6c40393229c4962d8bf113087ac630c1
HariPalempati <Palempati@PHANIs-MacBook-Pro-2.local>
2016-06-22 10:01:32 -0700

    Added Deck Class

---
+++ Deck_Shuffling/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Deck_Shuffling
+//
+//  Created by Hari Palempati on 6/22/16.
+//  Copyright © 2016 Hari Palempati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ Deck_Shuffling/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Deck_Shuffling
+//
+//  Created by Hari Palempati on 6/22/16.
+//  Copyright © 2016 Hari Palempati. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
