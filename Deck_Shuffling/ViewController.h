588e9de2cd793e5b8460e79a87bd2def1a12a936
Hari Palempati <Palempati@phanis-macbook-pro-2.local>
2016-06-22 09:45:46 -0700

    Initial Commit

---
+++ Deck_Shuffling/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deck_Shuffling
+//
+//  Created by Hari Palempati on 6/22/16.
+//  Copyright © 2016 Hari Palempati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Deck_Shuffling/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Deck_Shuffling
+//
+//  Created by Hari Palempati on 6/22/16.
+//  Copyright © 2016 Hari Palempati. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
